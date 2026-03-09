c5e0492c990bbbdeda6ecec86936525f7afeff18
Vincent Kriek <kriek.v@gmail.com>
2026-03-09 12:06:11 +0100

    add date range filtering

--- src/data_loader/load_huggingface.py
+++ src/data_loader/load_huggingface.py
@@ -2,7 +2,7 @@ from huggingface_hub import hf_hub_download
 import polars as pl
 
 
-def load_hf_lazyframe(repo_id, subfolder, filename):
+def load_hf_lazyframe(repo_id, subfolder, filename, min_date, max_date):
     path = hf_hub_download(
         repo_id=repo_id,
         filename=filename,
@@ -10,8 +10,16 @@ def load_hf_lazyframe(repo_id, subfolder, filename):
         repo_type="dataset",
     )
 
-    return pl.scan_csv(
-        path,
-        ignore_errors=True,
-        infer_schema_length=10000,
+    return (
+        pl.scan_csv(
+            path,
+            ignore_errors=True,
+            infer_schema_length=10000,
+        )
+        .with_columns(
+            pl.col("Date")
+            .str.replace(" UTC", "")
+            .str.strptime(pl.Datetime, "%Y-%m-%d %H:%M:%S", strict=False)
+        )
+        .filter(pl.col("Date").is_between(min_date, max_date))
     )
--- src/data_loader/main.py
+++ src/data_loader/main.py
@@ -6,36 +6,55 @@ from news_scraper import add_article_column_stream
 from summarize import add_summary_column
 import polars as pl
 from pathlib import Path
+from datetime import datetime
 
 
 load_dotenv()
+CHUNK_SIZE = 7  # chunk size in days
 
 
 def load_data():
     repo_id = os.environ["HF_REPO_ID"]
     subfolder = os.environ["HF_SUBFOLDER"]
+    min_date = datetime.strptime(os.environ["MIN_DATE"], "%Y-%m-%d")
+    max_date = datetime.strptime(os.environ["MAX_DATE"], "%Y-%m-%d")
 
     # load data into lazyframe
-    all_external_lf = load_hf_lazyframe(repo_id, subfolder, "All_external.csv")
-    nasdaq_lf = load_hf_lazyframe(repo_id, subfolder, "nasdaq_exteral_data.csv")
-
-    # processing of external news
-    all_external_lf = (
-        all_external_lf.pipe(add_article_column_stream)
-        .pipe(add_summary_column)
-        .pipe(remove_unneccessary_columns)
+    all_external_lf = load_hf_lazyframe(
+        repo_id, subfolder, "All_external.csv", min_date, max_date
+    )
+    nasdaq_lf = load_hf_lazyframe(
+        repo_id, subfolder, "nasdaq_exteral_data.csv", min_date, max_date
     )
 
+    # processing of external news
+    all_external_lf = load_external_data(all_external_lf)
+
     # processing of nasdaq news
-    nasdaq_lf = nasdaq_lf.pipe(add_summary_column).pipe(remove_unneccessary_columns)
+    nasdaq_lf = load_nasdaq_data(nasdaq_lf)
 
     # concat lazyframes
     news_lf = pl.concat([all_external_lf, nasdaq_lf])
 
-    print(news_lf.collect())
     news_lf.sink_parquet(Path("data/news_data_loader.parquet"), compression="zstd")
 
     return news_lf
+
+
+def load_external_data(lf):
+    return (
+        lf.pipe(add_article_column_stream)
+        .pipe(add_summary_column)
+        .pipe(remove_unneccessary_columns)
+    )
+
+
+def load_nasdaq_data(lf):
+    return lf.pipe(add_summary_column).pipe(remove_unneccessary_columns)
+
+
+def combine_chunks():
+    pass
 
 
 if __name__ == "__main__":
