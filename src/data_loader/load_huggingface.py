a67311d93c5a45ededcf8197c3f0c3c30d0557d5
Mike27122003 <dummyraheem@gmail.com>
2026-03-10 11:35:19 +0100

    Stock Price loader

--- src/data_loader/load_huggingface.py
+++ src/data_loader/load_huggingface.py
@@ -1,6 +1,64 @@
 from huggingface_hub import hf_hub_download
 import polars as pl
+import zipfile
+from pathlib import Path
 
+EXPECTED_COLS = ["date", "open", "high", "low", "close", "adj close", "volume"]
+
+def read_and_reorder_csv(csv_file: Path):
+    # Read CSV normally (let Polars infer types)
+    df = pl.read_csv(csv_file, infer_schema_length=10000)
+    if csv_file.name in ["A.csv", "AA.csv"]:
+        print(f"Preview of {csv_file.name}:")
+        print(df.head(5))  # collects first 5 rows and prints actual data
+
+    # Find which columns exist in this file
+    cols_in_file = df.columns
+    cols_to_select = [c for c in EXPECTED_COLS if c in cols_in_file]
+    if csv_file.name in ["A.csv", "AA.csv"]:
+        print(cols_to_select)
+
+    # Reorder the columns, fix the types
+    df = df.select(cols_to_select)
+    df = df.with_columns([
+        pl.col("date").cast(pl.Utf8),
+        pl.col("open").cast(pl.Float64),
+        pl.col("high").cast(pl.Float64),
+        pl.col("low").cast(pl.Float64),
+        pl.col("close").cast(pl.Float64),
+        pl.col("adj close").cast(pl.Float64),
+        pl.col("volume").cast(pl.Int64),
+    ])
+    
+    if csv_file.name in ["A.csv", "AA.csv"]:
+        print(f"Postview of {csv_file.name}:")
+        print(df.head(5))  # collects first 5 rows and prints actual data
+
+    return df
+
+def load_and_reorder_csvs(csv_files) -> pl.LazyFrame:
+    """
+    Scans all CSVs, reorders columns according to a set schema`,
+    and returns a single LazyFrame.
+    """
+    dfs = [read_and_reorder_csv(f) for f in csv_files]
+    # for i, df in enumerate(dfs):
+    #     print(df.collect_schema())
+    df_all = pl.concat(dfs).rename({"date": "Date"})
+    # lfs = []
+
+    # for i, csv_file in enumerate(csv_files):
+    #     lf = pl.scan_csv(csv_file, schema=PRICE_COL_SCHEMA)
+    #     # if i  < 2:
+    #     #     print(csv_file)
+    #     #     print(lf.columns)
+    #     #     print(lf.head(5).collect())
+    #     # lf = lf.select(expected_columns)
+    #     lfs.append(lf)
+     
+    # # print("-"*50)
+    # lf:pl.LazyFrame = pl.concat(lfs)
+    return pl.LazyFrame(df_all)
 
 def load_hf_lazyframe(repo_id, subfolder, filename, min_date, max_date):
     path = hf_hub_download(
@@ -17,6 +75,49 @@ def load_hf_lazyframe(repo_id, subfolder, filename, min_date, max_date):
             infer_schema_length=10000,
         )
         .with_columns(
+            pl.col("Date")
+            .str.replace(" UTC", "")
+            .str.strptime(pl.Datetime, "%Y-%m-%d %H:%M:%S", strict=False)
+        )
+        .filter(pl.col("Date").is_between(min_date, max_date))
+    )
+
+def load_hf_prices_lazyframe(repo_id, subfolder, filename, min_date, max_date) -> pl.LazyFrame:
+
+    # Download zip from Hugging Face
+    zip_path = hf_hub_download(
+        repo_id=repo_id,
+        filename=filename,
+        subfolder=subfolder,
+        repo_type="dataset",
+    )
+
+    # Extract zip next to the downloaded file
+    extract_dir = Path(zip_path).with_suffix("")
+    if not extract_dir.exists():
+        with zipfile.ZipFile(zip_path, "r") as z:
+            z.extractall(extract_dir)
+
+    # Find all CSV files
+    csv_files = [
+        p for p in extract_dir.rglob("*.csv")
+        if not p.name.startswith("._") and "__MACOSX" not in str(p)
+    ]
+    # print(csv_files[0])
+
+    lf = load_and_reorder_csvs(csv_files) # re-order columns in a consistent schema
+    # print(lf.head())
+
+    # Scan all CSVs lazily
+    # lf = pl.scan_csv(
+    #     [str(p) for p in csv_files],
+    #     ignore_errors=True,
+    #     infer_schema_length=10000,
+    # )
+
+    # Process Date column and filter
+    return (
+        lf.with_columns(
             pl.col("Date")
             .str.replace(" UTC", "")
             .str.strptime(pl.Datetime, "%Y-%m-%d %H:%M:%S", strict=False)
--- src/data_loader/main.py
+++ src/data_loader/main.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 import os
-from load_huggingface import load_hf_lazyframe
+from load_huggingface import load_hf_lazyframe, load_hf_prices_lazyframe
 from clean_data import remove_unneccessary_columns
 from news_scraper import add_article_column_stream
 from summarize import add_summary_column
@@ -15,25 +15,86 @@ BATCH_DAYS = 30  # batch size in days
 
 
 def load_data():
-    repo_id = os.environ["HF_REPO_ID"]
-    subfolder = os.environ["HF_SUBFOLDER"]
-    min_date = datetime.strptime(os.environ["MIN_DATE"], "%Y-%m-%d")
-    max_date = datetime.strptime(os.environ["MAX_DATE"], "%Y-%m-%d")
+    # repo_id = os.environ["HF_REPO_ID"]
+    # subfolder = os.environ["HF_SUBFOLDER"]
+    # min_date = datetime.strptime(os.environ["MIN_DATE"], "%Y-%m-%d")
+    # max_date = datetime.strptime(os.environ["MAX_DATE"], "%Y-%m-%d")
+
+    repo_id = "Zihan1004/FNSPID"
+    subfolder = "Stock_price"
+    min_date = datetime.strptime("2019-01-01", "%Y-%m-%d")
+    max_date = datetime.strptime("2020-01-01", "%Y-%m-%d")
+
+    # load stock price data
+    prices_lf = load_hf_prices_lazyframe(
+        repo_id, subfolder, "full_history.zip", min_date, max_date
+    )
+    
+    # p = Path("C:/Users/micha/.cache/huggingface/hub/datasets--Zihan1004--FNSPID/snapshots/bf9189c41527198897d1af3e17b1a0095279fc45/Stock_price/full_history/full_history")
+    # test_file = p / "zyxi.csv"
+    # test_file2 = p / "zumz.csv"
+    
+    prices_lf = load_price_data(prices_lf, min_date, max_date)
 
     # load data into lazyframe
-    all_external_lf = load_hf_lazyframe(
-        repo_id, subfolder, "All_external.csv", min_date, max_date
+    # all_external_lf = load_hf_lazyframe(
+    #     repo_id, subfolder, "All_external.csv", min_date, max_date
+    # )
+    # nasdaq_lf = load_hf_lazyframe(
+    #     repo_id, subfolder, "nasdaq_exteral_data.csv", min_date, max_date
+    # )
+
+    # # processing of external news
+    # all_external_lf = load_external_data(all_external_lf, min_date, max_date)
+
+    # # processing of nasdaq news
+    # nasdaq_lf = load_nasdaq_data(nasdaq_lf, min_date, max_date)
+
+# def check_column_order(folder):
+#     reference = None
+
+#     for path in Path(folder).glob("*.csv"):
+#         with open(path) as f:
+#             cols = tuple(f.readline().strip().split(","))
+
+#         if reference is None:
+#             reference = cols
+#             continue
+
+#         if cols != reference:
+#             print(f"\n{path} has different column order")
+#             print("expected:", reference)
+#             print("got     :", cols)
+
+# def enforce_schema(lf: pl.DataFrame):
+#     # col_order = ["date", "volume", "open", "high", "low", "close", "adj close"]
+#     ordered_lf = lf.select("date", "volume", "open", "high", "low", "close", "adj close")
+#     print(ordered_lf.collect_schema())
+#     return ordered_lf
+
+def load_price_data(lf, start_date, end_date):
+    total_batches = ceil((end_date - start_date).days / BATCH_DAYS)
+    existing_ranges = get_existing_ranges(Path("data/loader/batches/price"))
+
+    batch_lf, current_date, next_current_date, batch_start, batch_end = create_batch(
+        lf, start_date, end_date, existing_ranges
     )
-    nasdaq_lf = load_hf_lazyframe(
-        repo_id, subfolder, "nasdaq_exteral_data.csv", min_date, max_date
-    )
 
-    # processing of external news
-    all_external_lf = load_external_data(all_external_lf, min_date, max_date)
+    with tqdm(total=total_batches, desc="Processing price batches") as pbar:
+        while current_date < end_date:
+            if batch_lf is not None:
+                out_path = Path(
+                    f"data/loader/batches/price/{batch_start.date()}_{batch_end.date()}.parquet"
+                )
+                out_path.parent.mkdir(parents=True, exist_ok=True)
+                batch_lf.sink_parquet(out_path, compression="zstd")
 
-    # processing of nasdaq news
-    nasdaq_lf = load_nasdaq_data(nasdaq_lf, min_date, max_date)
 
+            batch_lf, current_date, next_current_date, batch_start, batch_end = (
+                create_batch(lf, next_current_date, end_date, existing_ranges)
+            )
+
+            pbar.update(1)
 
 def load_external_data(lf, start_date, end_date):
     total_batches = ceil((end_date - start_date).days / BATCH_DAYS)
